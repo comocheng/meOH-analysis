44fcff608e8b9745b35eab1351b68d839315e523
chrisbneu <blais.ch@husky.neu.edu>
2021-02-28 08:56:59 -0500

    added in data analysis notebook, updated reactor script to run a commit and record commit details

--- cantera_simulations/RMG_wDeut/Grabow_array_job.sh
+++ cantera_simulations/RMG_wDeut/Grabow_array_job.sh
@@ -6,12 +6,12 @@
 #SBATCH --mem=20GB
 #SBATCH --exclude=c5003
 #SBATCH --job-name=grabDeut
-#SBATCH --output=deut_grabow.%a.log
-#SBATCH --error=deut_grabow.%a.slurm.log
+#SBATCH --output=logs/deut_grabow.%a.log
+#SBATCH --error=logs/deut_grabow.%a.slurm.log
 #SBATCH --partition=short
 
 #an array for the job.
-#SBATCH --array=1-36
+#SBATCH --array=1-107
 
 
 ####################################################
--- cantera_simulations/RMG_wDeut/Grabow_reactor_script.py
+++ cantera_simulations/RMG_wDeut/Grabow_reactor_script.py
@@ -21,7 +21,7 @@ from collections import defaultdict
 import git
 
 
-def save_flux_diagrams(*phases, suffix=''):
+def save_flux_diagrams(*phases, suffix='', timepoint=''):
     import pandas as pd
     import numpy as np
     import time
@@ -45,10 +45,10 @@ def save_flux_diagrams(*phases, suffix=''):
 
             diagram = ct.ReactionPathDiagram(phase_object, element)
             diagram.title = f'Reaction path diagram following {element} in {phase}'
-            diagram.label_threshold = 0.01
+            diagram.label_threshold = 0.001
 
-            dot_file = f"{suffix}/reaction_path_{element}_{phase}.dot"
-            img_file = f"{suffix}/reaction_path_{element}_{phase}.png"
+            dot_file = f"{suffix}/reaction_path_{element}_{phase}_{timepoint}.dot"
+            img_file = f"{suffix}/reaction_path_{element}_{phase}_{timepoint}.png"
             dot_bin_path = '/Users/blais.ch/anaconda3/pkgs/graphviz-2.40.1-hefbbd9a_2/bin/dot'
             img_path = os.path.join(os.getcwd(), img_file)
             diagram.write_dot(dot_file)
@@ -100,12 +100,13 @@ def run_reactor(cti_file, t_array=[528], p_array=[75], v_array=[0.00424], h2_arr
 
     # set initial temps, pressures, concentrations
     temp = settings[array_i][0] # kelvin
+    temp_str = str(temp)[0:3]
     pressure = settings[array_i][1]*ct.one_atm # Pascals
 
 
     X_h2 = settings[array_i][3]
-    x_h2_str = str(X_h2)[0:2]
-    x_CO_CO2_str = str([array_i][4])[0:2]
+    x_h2_str = str(X_h2)[0:3].replace(".", "_")
+    x_CO_CO2_str = str(settings[array_i][4])[0:3].replace(".", "_")
     # Per Grabow experiments, add in H2O for X=0.75 H2 run
     if X_h2 == 0.75:
         X_h2o = 0.05
@@ -201,39 +202,58 @@ def run_reactor(cti_file, t_array=[528], p_array=[75], v_array=[0.00424], h2_arr
     # temp_str = '%s' % '%.3g' % tempn
 
     cat_area_str = '%s' % '%.3g' % cat_area
-    results_path = os.path.dirname(os.path.abspath(__file__)) + 
-                                  f'/{git_sha}_{git_msg}/{reactor_type_str}/results'
-    flux_path = os.path.dirname(os.path.abspath(__file__)) + 
-                               f'/{git_sha}_{git_msg}/{reactor_type_str}/flux_diagrams/{x_h2_str}/{x_CO_CO2_str}'
+    results_path = os.path.dirname(os.path.abspath(__file__)) + f'/{git_sha}_{git_msg}/{reactor_type_str}/{temp_str}/results'
+    flux_path = os.path.dirname(os.path.abspath(__file__)) + f'/{git_sha}_{git_msg}/{reactor_type_str}/{temp_str}/flux_diagrams/{x_h2_str}/{x_CO_CO2_str}'
     try:  
-        os.mkdir(results_path)  
-        os.mkdir(flux_path)
+        os.makedirs(results_path, exist_ok = True)  
     except OSError as error:  
         print(error) 
     
-    output_filename = results_path + f'/Spinning_basket_area_{cat_area_str}_energy_{energy}_temp_{temp}.csv'
+    try:  
+        os.makedirs(flux_path, exist_ok = True)
+    except OSError as error:  
+        print(error)
+
+    gas_ROP_str = [i + ' ROP [kmol/m^3 s]' for i in gas.species_names]
+    
+    #surface ROP reports gas and surface ROP. these values might be redundant, not sure. 
+    
+    gas_surf_ROP_str = [i + ' surface ROP [kmol/m^2 s]' for i in gas.species_names]
+    surf_ROP_str = [i + ' ROP [kmol/m^2 s]' for i in surf.species_names]
+    
+    gasrxn_ROP_str = [i + ' ROP [kmol/m^3 s]' for i in gas.reaction_equations()] 
+    surfrxn_ROP_str =  [i + ' ROP [kmol/m^2 s]' for i in surf.reaction_equations()] 
+    
+    
+    output_filename = results_path + f'/Spinning_basket_area_{cat_area_str}_energy_{energy}_temp_{temp}_h2_{x_h2_str}_COCO2_{x_CO_CO2_str}.csv'
     outfile = open(output_filename,'w')
     writer = csv.writer(outfile)
     writer.writerow(['T (C)', 'P (atm)', 'V (M^3/s)', 'X_co initial','X_co2 initial','X_h2 initial','X_h2o initial',
-                    'CO2/(CO2+CO)','(CO+CO2/H2)', 'T (C) final', 'Rtol', 'Atol'] + gas.species_names + surf.species_names,)
+                    'CO2/(CO2+CO)','(CO+CO2/H2)', 'T (C) final', 'Rtol', 'Atol'] + gas.species_names + surf.species_names + gas_ROP_str + gas_surf_ROP_str + surf_ROP_str + gasrxn_ROP_str + surfrxn_ROP_str)
 
     t = 0.0
-    dt = 0.01
+    dt = 0.1
 
     # run the simulation
+    first_run = True
 
-    while t < 10000.0:
-    # while t < 1000000.0:
+    while t < 1e5:
+    # save flux diagrams at beginning of run
+        if first_run == True:
+            save_flux_diagrams(gas,suffix=flux_path, timepoint='beginning')
+            save_flux_diagrams(surf,suffix=flux_path, timepoint='beginning')
+            first_run = False
         t += dt
         sim.advance(t)
+        if t%10 < 0.01:
+            writer.writerow([temp, pressure, volume_flow, X_co, X_co2, X_h2, X_h2o, co2_ratio, h2_ratio, gas.T, sim.rtol, sim.atol] +
+                list(gas.X) + list(surf.X) + list(gas.net_production_rates) + list(surf.net_production_rates) + list(gas.net_rates_of_progress) + list(surf.net_rates_of_progress))
     
-    writer.writerow([temp, pressure, volume_flow, X_co, X_co2, X_h2, X_h2o, co2_ratio, h2_ratio, gas.T, sim.rtol, sim.atol] +
-                    list(gas.X) + list(surf.X),)
     outfile.close()
 
-    # save flux diagrams
-    save_flux_diagrams(gas,suffix=flux_path)
-    save_flux_diagrams(surf,suffix=flux_path)
+    # save flux diagrams at the end of the run
+    save_flux_diagrams(gas, suffix=flux_path, timepoint='end')
+    save_flux_diagrams(surf, suffix=flux_path, timepoint='end')
     return 
 
 
@@ -241,18 +261,12 @@ def run_reactor(cti_file, t_array=[528], p_array=[75], v_array=[0.00424], h2_arr
 # Input Parameters for combustor
 #######################################################################
 
-# git checkout
-# repo.git.checkout('master')
-sha = repo.head.commit
-msg = repo.head.commit.message
-print(sha,': ', msg)
-
 # filepath for writing files
 # cti_file = os.path.dirname(os.path.abspath(__file__)) +'/chem_annotated.cti'
 cti_file = '/work/westgroup/ChrisB/meoh-synthesis_RMG/meOH-synthesis/base/cantera/chem_annotated.cti'
-print(cti_file)
+
 # Reactor settings arrays for run
-Temps = [483.7-273.25, 499.3-273.25, 516.7-273.25]
+Temps = [400, 500, 600]
 Pressures = [15,30,50,75]
 volume_flows = [0.00424,0.0106,0.02544]
 
@@ -267,5 +281,5 @@ H2_fraction = [0.8,0.5,0.95,0.75]
 #CO2/CO
 CO_CO2_ratio = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9] 
 
-run_reactor(cti_file=cti_file, reactor_type=3, h2_array=H2_fraction, co2_array=CO_CO2_ratio)
+run_reactor(cti_file=cti_file, t_array=Temps, reactor_type=3, h2_array=H2_fraction, co2_array=CO_CO2_ratio)
 
