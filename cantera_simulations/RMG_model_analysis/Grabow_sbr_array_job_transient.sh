887b5fbd5b78a165e22124922cfbacc37679e8a2
Chris B <56306881+ChrisBNEU@users.noreply.github.com>
2021-09-08 16:30:10 -0400

    added scripts for transient sbr run

---
+++ cantera_simulations/RMG_model_analysis/Grabow_sbr_array_job_transient.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --time=1:00:00
+#SBATCH --mem=20GB
+#SBATCH --exclude=c5003
+#SBATCH --job-name=grabDeut
+#SBATCH --output=logs/deut_grabow.%a.log
+#SBATCH --error=logs/deut_grabow.%a.slurm.log
+#SBATCH --partition=short
+#SBATCH --mail-user=blais.ch@northeastern.edu 
+#SBATCH --mail-type=FAIL,END
+
+#an array for the job. usually 107, testing use 2
+#SBATCH --array=1-107
+
+
+####################################################
+source activate rmg_env
+python -u Grabow_sbr_script_transient.py
