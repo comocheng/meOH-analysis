ccfeb93bfa3f01c3e779962e1481ee508b791620
Sevy Harris <harris.se@northeastern.edu>
2021-09-24 09:51:57 -0400

    analyze_temperatures now takes the cti path as an argument and creates the csv in the same folder

--- cantera_simulations/uncertainty/analyze_temperatures.py
+++ cantera_simulations/uncertainty/analyze_temperatures.py
@@ -1,23 +1,31 @@
+import os
+import sys
 import numpy as np
 import pandas as pd
 import itertools
 from multiprocessing import Pool
 from min_sbr import MinSBR
-import os
 # import time
 
 
+if len(sys.argv < 2):
+    raise ValueError("Incorrect usage. Must path the cantera model file as an argument to this analysis script")
+
+if not os.path.exists(sys.argv[2]):
+    raise OSError(f"Path to the cantera model file does not exist: {sys.argv[2]}")
+
 # start = time.time()
 
-rmg_model_folder = "/home/moon/methanol/perturb_5000/run_0000/"
+# rmg_model_folder = "/home/moon/methanol/perturb_5000/run_0000/"
 # rmg_model_folder = "/home/sevy/methanol/perturb_5000/run_0000/"
 # rmg_model_folder = "/scratch/westgroup/methanol/perturb_5000/run_0000/"
 
-cti_file_path = "/home/moon/methanol/perturb_5000/run_0000/cantera/chem_annotated.cti"
+# cti_file_path = "/home/moon/methanol/perturb_5000/run_0000/cantera/chem_annotated.cti"
 # cti_file_path = "/home/sevy/methanol/perturb_5000/run_0000/cantera/chem_annotated.cti"
 # cti_file_path = "/scratch/westgroup/methanol/perturb_5000/run_0000/cantera/chem_annotated.cti"
 
-csv_path = os.path.join(rmg_model_folder, "ct_analysis.csv")
+cti_file_path = sys.argv[2]
+csv_path = os.path.join(os.path.dirname(cti_file_path), "ct_analysis.csv")
 
 temperatures = np.linspace(400.0, 700.0, 20)
 pressures = np.linspace(30.0, 75.0, 1)
